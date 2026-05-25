aec78d26b32c385a6f27ead58bec2b896227d0df
s240711014 <37koukou@gmail.com>
2026-05-26 01:31:46 +0900

    変更

--- mapEdit.js
+++ mapEdit.js
@@ -1,3 +1,4 @@
+let selectChip ="chipA.png"; //画像の初期値
 function init(){
     let b = document.getElementById("board");
     for (let i = 0; i < 8; i++){
@@ -13,16 +14,20 @@ function init(){
             td.appendChild(img);
         }
         b.appendChild(tr);
+    //画像(classで管理して、選択できるようにした。)
+    let chips = document.getElementsByClassName("selectChip");
+    for(let i = 0; i < chips.length; i++){
+        chips[i].onclick = select; 
+    }
+
     }
 }
-
+//マップ内画像をクリックの処理
 function clicked(e) {
     document.getElementById("info").textContent = e.target.id + "clicked";
-    if(e.target.src.includes("chipA.png")){
-        e.target.src = "chipA1.png"; 
-    }
-    else{
-        e.target.src = "chipA.png";     //再度押したときに元の画像に戻るようにする
-    }
-
+    document.getElementById(e.target.id).src =selectChip;
+}
+//画像の選択時の処理
+function select(e){
+    selectChip=e.target.src;
 }
